c5a8becb10dbf2536e1bf1d59da26b7b54c29ded
Marcelo Martim Marques <marcelo.martim@gmail.com>
2015-11-21 21:57:18 -0200

    Initial version

---
+++ README.txt
@@ -0,0 +1,9 @@
+This script was based on info available at:
+
+http://www.arduinoecia.com.br/2015/02/sensor-de-temperatura-dht22-raspberry-pi.html
+
+This script should be run as root:
+
+sudo ./dht22.py
+
+
---
+++ dht22.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Program : Raspberry 2 and DHT22 sensor
+# Author : Arduino e Cia
+
+# Load libraries
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+
+# Set sensor type
+sensor = Adafruit_DHT.DHT22
+
+GPIO.setmode(GPIO.BOARD)
+
+# Set GPIO port connected to sensor
+pino_sensor = 4
+
+# Read data from sensor
+umid, temp = Adafruit_DHT.read_retry(sensor, pino_sensor);
+
+# Show data if read was succesful
+if umid is not None and temp is not None:
+  print ("Temperature = {0:0.1f}  Humidity = {1:0.1f}\n").format(temp, umid);
+else:
+  # Show error message if read was not ok
+  print("Failed to read sensor data !!!")
